80f340634f30268cca5b72bd6ba37b3cc999bb8c
Eric Rabun <errabun@gmail.com>
2021-04-12 21:09:49 -0600

    Construct ingredient-test.js file

---
+++ test/ingredient-test.js
@@ -0,0 +1,34 @@
+import { expect } from 'chai';
+
+import Ingredient from '../src/ingredient';
+import ingredientData from '../src/data/ingredient-test-data';
+
+describe('Ingredient', function() {
+  let ingredient;
+  let ingredientInfo;
+
+  beforeEach(function() {
+    ingredientInfo = ingredientData[0];
+    ingredient = new Ingredient(ingredientInfo);
+  })
+
+  it('is a function', function() {
+    expect(Ingredient).to.be.a('function');
+  });
+
+  it('should be an instance of Ingredient', function() {
+    expect(ingredient).to.be.an.instanceof(Ingredient);
+  });
+
+  it('should be able to store an id', function() {
+    expect(ingredient[0].id).to.equal(20081);
+  });
+
+  it('should be able to store the name of the ingredient', function() {
+    expect(ingredient[0].name).to.equal("wheat flour");
+  });
+
+  it('should be able to store the estimated cost in cents', function() {
+    expect(ingredient[0].estimatedCostInCents).to.equal(142);
+  });
+})
