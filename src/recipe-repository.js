8a1031afa8bae66bde109427785294c51b785fde
Eric Rabun <errabun@gmail.com>
2021-04-13 17:56:15 -0600

    Create filterRecipesByName method

--- src/recipe-repository.js
+++ src/recipe-repository.js
@@ -4,6 +4,17 @@ class RecipeRepository {
   constructor(recipeData) {
     this.recipes = recipeData.map(recipe => new Recipe(recipe));
   }
+
+  filterRecipes(quality, filterValues) {
+    // let recipeNames = this.recipes.map(recipe => recipe[quality]);
+    // return recipeNames.filter(name => {
+    //   filterValues.every(name)
+    // })
+  }
+
+  filterRecipesByName(name) {
+    return this.recipes.filter(recipe => recipe.name.includes(name));
+  }
 }
 
 export default RecipeRepository;
--- test/recipe-repository-test.js
+++ test/recipe-repository-test.js
@@ -24,4 +24,16 @@ describe('RecipeRepository', function() {
   it('should create a new instance of Recipe for each recipe', function() {
     expect(recipe.recipes[0]).to.be.an.instanceof(Recipe);
   });
+
+  it('should be able to filter recipes based on ingredients', function() {
+    expect(recipe.filterRecipes('ingredients', 'sriracha')).to.deep.equal();
+  })
+
+  it('should be able to filter recipes based on tag(s)', function() {
+    expect(recipe.filterRecipes()).to.deep.equal();
+  })
+
+  it('should be able to filter recipes based on name', function() {
+    expect(recipe.filterRecipes()).to.deep.equal();
+  })
 })
