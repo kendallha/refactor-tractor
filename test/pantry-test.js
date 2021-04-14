8db29a6cd555ca838ca874df5b4493d157ba56e7
Eric Rabun <errabun@gmail.com>
2021-04-13 21:26:44 -0600

    Refactor pantry-test

--- test/pantry-test.js
+++ test/pantry-test.js
@@ -29,92 +29,7 @@ describe('Pantry', function() {
   });
 
   it('should contain an array of ingredients', function() {
-    expect(pantry.pantryIngredients).to.deep.eq([
-      {
-        "ingredient": 1009016,
-        "amount": 2
-      },
-      {
-        "ingredient": 11477,
-        "amount": 1
-      },
-      {
-        "ingredient": 93820,
-        "amount": 1
-      },
-      {
-        "ingredient": 11297,
-        "amount": 3
-      },
-      {
-        "ingredient": 11547,
-        "amount": 5
-      },
-      {
-        "ingredient": 1082047,
-        "amount": 5
-      },
-      {
-        "ingredient": 1032050,
-        "amount": 1
-      },
-      {
-        "ingredient": 20081,
-        "amount": 4
-      },
-      {
-        "ingredient": 11215,
-        "amount": 2
-      },
-      {
-        "ingredient": 10514037,
-        "amount": 2
-      },
-      {
-        "ingredient": 2047,
-        "amount": 2
-      },
-      {
-        "ingredient": 12179,
-        "amount": 1
-      },
-      {
-        "ingredient": 1123,
-        "amount": 4
-      },
-      {
-        "ingredient": 11282,
-        "amount": 5
-      },
-      {
-        "ingredient": 9016,
-        "amount": 4
-      },
-      {
-        "ingredient": 14003,
-        "amount": 4
-      },
-      {
-        "ingredient": 6194,
-        "amount": 2
-      },
-      {
-        "ingredient": 16112,
-        "amount": 1
-      },
-      {
-        "ingredient": 6172,
-        "amount": 3
-      },
-      {
-        "ingredient": 2044,
-        "amount": 2
-      },
-      {
-        "ingredient": 2050,
-        "amount": 3
-      }
-    ])
+    expect(pantry.pantryIngredients).to.deep.eq(user.pantry)
   });
 
   it('should return true if pantry has ingredients for recipe', function() {
@@ -125,7 +40,7 @@ describe('Pantry', function() {
     expect(pantry.checkIngredientsMeal(recipe.recipes[0])).to.eq(false);
   });
 
-  it.only('should return an array of missing ingredients for recipe', function() {
+  it('should return an array of missing ingredients for recipe', function() {
     expect(pantry.findMissingIngredientsMeal(recipe.recipes[1])).to.deep.eq([{ingredient: "apple cider", amount: 0.5}])
   });
   //
